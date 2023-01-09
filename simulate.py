dc2e7992338988278443e3f8069b08934eef6a75
eiokonji <ebubeokonji2024@u.northwestern.edu>
2023-01-09 11:26:59 -0600

    stepping through the world

--- simulate.py
+++ simulate.py
@@ -1,5 +1,13 @@
 import pybullet as p
+import time
 
 physicsCLient = p.connect(p.GUI)
+
+# opening the window using for loop 1000 times
+iterations = 1000
+for i in range(iterations):
+    p.stepSimulation()
+    time.sleep(1/60)
+    print(i)
 
 p.disconnect()
