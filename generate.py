e263164a41e57c53e0fa02814bcff478ceb7421b
eiokonji <ebubeokonji2024@u.northwestern.edu>
2023-01-09 17:59:06 -0600

    adding box.sdf

--- generate.py
+++ generate.py
@@ -1,1 +1,9 @@
-pass
+import pyrosim.pyrosim as pyrosim
+
+# tells pyrosim name of object file
+pyrosim.Start_SDF("box.sdf")
+
+# sets the initial position and size of a cube in object file. measured in metres.
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[1,1,1])
+
+
