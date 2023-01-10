294e367b6a292a5ba0f6208669238a45b456f930
eiokonji <ebubeokonji2024@u.northwestern.edu>
2023-01-09 20:24:04 -0600

    created, separated robot and world links

--- generate.py
+++ generate.py
@@ -1,25 +1,31 @@
 import pyrosim.pyrosim as pyrosim
 
+# sets the initial position and size of a cube in object file. measured in metres.
+# size parameters
+length = 1
+width = 1
+height = 1
+
+# position parameters
+x = 0               # red
+y = 0               # green
+z = 0.5             # blue
+
+
 def Create_World():
     # tells pyrosim name of object file
     pyrosim.Start_SDF("world.sdf")
-
-    # sets the initial position and size of a cube in object file. measured in metres.
-    # size parameters
-    length = 1
-    width = 1
-    height = 1
-
-    # position parameters
-    x = 0               # red
-    y = 0               # green
-    z = 0.5             # blue
-
     #  create a single block at origin
-    pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length, width, height])
+    pyrosim.Send_Cube(name="Box", pos=[x-2,y+2,z], size=[length, width, height])
 
     pyrosim.End()
 
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
 
+    pyrosim.Send_Cube(name="Torso", pos=[x,y,z], size=[length, width, height])
 
+    pyrosim.End()
 
+Create_World()
+Create_Robot()
--- simulate.py
+++ simulate.py
@@ -7,6 +7,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
 
 # opening the window using for loop 1000 times
 iterations = 1000
