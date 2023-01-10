95a6b420314b4faebdc0b0ed74afaf1cf9528782
eiokonji <ebubeokonji2024@u.northwestern.edu>
2023-01-09 18:00:44 -0600

    generated onelink

--- generate.py
+++ generate.py
@@ -6,4 +6,4 @@ pyrosim.Start_SDF("box.sdf")
 # sets the initial position and size of a cube in object file. measured in metres.
 pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[1,1,1])
 
-
+pyrosim.End()
