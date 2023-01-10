ed498f07ee1790f203d297d840a5ccefa75be6e6
eiokonji <ebubeokonji2024@u.northwestern.edu>
2023-01-09 18:42:12 -0600

    simulating the floor

--- simulate.py
+++ simulate.py
@@ -1,8 +1,12 @@
 import pybullet as p
+import pybullet_data
 import time
 
 physicsCLient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
 p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
 
 # opening the window using for loop 1000 times
 iterations = 1000
