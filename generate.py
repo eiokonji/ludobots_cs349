bc627ae5cc4304470c0081437d2a41f325cd7862
eiokonji <ebubeokonji2024@u.northwestern.edu>
2023-01-09 20:15:13 -0600

    defined create_world() function

--- generate.py
+++ generate.py
@@ -1,20 +1,25 @@
 import pyrosim.pyrosim as pyrosim
 
-# tells pyrosim name of object file
-pyrosim.Start_SDF("world.sdf")
+def Create_World():
+    # tells pyrosim name of object file
+    pyrosim.Start_SDF("world.sdf")
 
-# sets the initial position and size of a cube in object file. measured in metres.
-# size parameters
-length = 1
-width = 1
-height = 1
+    # sets the initial position and size of a cube in object file. measured in metres.
+    # size parameters
+    length = 1
+    width = 1
+    height = 1
 
-# position parameters
-x = 0               # red
-y = 0               # green
-z = 0.5             # blue
+    # position parameters
+    x = 0               # red
+    y = 0               # green
+    z = 0.5             # blue
 
-#  create a single block at origin
-pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length, width, height])
+    #  create a single block at origin
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length, width, height])
 
-pyrosim.End()
+    pyrosim.End()
+
+
+
+
