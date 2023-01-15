d5ff08a34a785cf6c320b68a91a1caa2de70075e
eiokonji <ebubeokonji2024@u.northwestern.edu>
2023-01-14 22:30:15 -0600

    added sensor to backleg

--- simulate.py
+++ simulate.py
@@ -1,3 +1,4 @@
+import pyrosim.pyrosim as pyrosim
 import pybullet as p
 import pybullet_data
 import time
@@ -9,11 +10,17 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 
+# setup for sensors
+pyrosim.Prepare_To_Simulate(robotId)
+
 # opening the window using for loop 1000 times
 iterations = 3000
 p.loadSDF("world.sdf")
 for i in range(iterations):
     p.stepSimulation()
     time.sleep(1/60)
+
+# adding sensors
+backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 
 p.disconnect()
