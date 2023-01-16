89e658dfd45397d30aa461457b25aa4b62b2ba21
eiokonji <ebubeokonji2024@u.northwestern.edu>
2023-01-16 05:27:46 -0600

    generated, plotted sinusoidal oscillations (-1, +1)

--- analyze.py
+++ analyze.py
@@ -4,10 +4,12 @@ import numpy
 # load date, numpy
 backLegSensorValues = numpy.load("./data/backlegsensor.npy")
 frontLegSensorValues = numpy.load("./data/frontlegsensor.npy")
+targetAngles = numpy.load("./data/targetangles.npy")
 
 # visualize data, matplotlib
-mp.plot(backLegSensorValues, label="Back Leg", linewidth=1.2)
-mp.plot(frontLegSensorValues, label="Front Leg", lw= 0.8)
+# mp.plot(backLegSensorValues, label="Back Leg", linewidth=1.2)
+# mp.plot(frontLegSensorValues, label="Front Leg", lw= 0.8)
+mp.plot(numpy.sin(targetAngles), label="Target Angles")
 
-mp.legend(loc='upper right')
+mp.legend(loc='best')
 mp.show()
--- simulate.py
+++ simulate.py
@@ -7,40 +7,53 @@ import time
 physicsCLient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-p.setGravity(0,0,-9.8)
+p.setGravity(0, 0, -9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 
 # set up pyrosim
 pyrosim.Prepare_To_Simulate(robotId)
 
+# iterations for for-loop
+iterations = 1000
+
 # storing sensor values, numpy
-backLegSensorValues = numpy.zeros(1000)
-frontLegSensorValues = numpy.zeros(1000)
+backLegSensorValues = numpy.zeros(iterations)
+frontLegSensorValues = numpy.zeros(iterations)
+
+# generate vector of sinusoidally varying values
+targetAngles = numpy.linspace(0, numpy.pi * 2, iterations)
 
 # opening the window using for loop 1000 times
-iterations = 1000
 p.loadSDF("world.sdf")
 for i in range(iterations):
     p.stepSimulation()
-    
+
     # add sensors
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link(
+        "FrontLeg")
 
     # simulating motors
     pyrosim.Set_Motor_For_Joint(
-        bodyIndex = robotId,
-        jointName = b'Torso_BackLeg',
-        controlMode = p.POSITION_CONTROL,
-        targetPosition = 0.0,
-        maxForce = 500)
+        bodyIndex=robotId,
+        jointName=b'Torso_BackLeg',
+        controlMode=p.POSITION_CONTROL,
+        targetPosition=-numpy.pi/8,
+        maxForce=500)
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex=robotId,
+        jointName=b'Torso_FrontLeg',
+        controlMode=p.POSITION_CONTROL,
+        targetPosition=numpy.pi/8,
+        maxForce=500)
 
     time.sleep(1/60)
 
-# saving sensor data to a file
+# saving sensor data, sine values to a file
 numpy.save('data/backlegsensor', backLegSensorValues)
 numpy.save('data/frontlegsensor', frontLegSensorValues)
+numpy.save('data/targetangles', targetAngles)
 
 print("back", backLegSensorValues)
 print("front", frontLegSensorValues)
