705b844ff284452988e7981f38448ae0fa9fa156
eiokonji <ebubeokonji2024@u.northwestern.edu>
2023-01-30 18:38:49 -0600

    created the hill climber

--- search.py
+++ search.py
@@ -1,9 +1,10 @@
 import os
+from hillclimber import HILL_CLIMBER
 
-for i in range(5):
-    # executes the generate.py, simulate.py files
-    os.system("python3 generate.py")
-    os.system("python3 simulate.py")
+# for i in range(5):
+#     # executes the generate.py, simulate.py files
+#     os.system("python3 generate.py")
+#     os.system("python3 simulate.py")
 
-
+hc = HILL_CLIMBER()
     
