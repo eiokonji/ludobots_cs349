07fefbbf9b78fc79a053d156c307993bbb7194bd
eiokonji <ebubeokonji2024@u.northwestern.edu>
2023-01-31 23:04:52 -0600

    broke the simulation intentionally

--- solution.py
+++ solution.py
@@ -61,7 +61,8 @@ class SOLUTION:
     def Create_Body(self):
         pyrosim.Start_URDF("body.urdf")
         # creating the robot
-        pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[length, width, height])
+        # pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[length, width, height])
+        pyrosim.Send_Cube(name="Torso", pos=[0, 0, 1], size=[length, width, height])
 
         pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[1, 0, 1])
         pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[length, width, height])
